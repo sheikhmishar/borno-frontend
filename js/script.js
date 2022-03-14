257ae3793b67e95071b202a658ce873807ca08a0
Syed Mishar Newaz <misharex.sezan@gmail.com>
2022-03-14 19:25:21 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,8 @@
+$(window).on("load", function() {
+  $("#preloader").fadeOut();
+
+  var divYear = $('#year'),
+    year = new Date().getFullYear().toString()
+  if (year != '2021') divYear.text('2021-'.concat(year))
+  else divYear.text(year)
+});
