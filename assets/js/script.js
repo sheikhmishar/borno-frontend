edfa94392f80ae9dc84424ddcd701a62d36dc36f
Syed Mishar Newaz <misharex.sezan@gmail.com>
2022-04-03 17:56:57 +0600

    theme toggle pink throughout the site

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,12 @@
 $(window).on("load", function() {
   $("#preloader").fadeOut();
 
-  var divYear = $('#year'),
-    year = new Date().getFullYear().toString()
-  if (year != '2021') divYear.text('2021-'.concat(year))
-  else divYear.text(year)
+  var divYear = $("#year"),
+    year = new Date().getFullYear().toString();
+  if (year != "2021") divYear.text("2021-".concat(year));
+  else divYear.text(year);
+
+  $("#dark-toggler,#dark-toggler-mobile").click(function() {
+    $("body").toggleClass("theme-pink");
+  });
 });
