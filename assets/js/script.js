d826576593aba4aa0d17415153cf403a35e88578
Syed Mishar Newaz <misharex.sezan@gmail.com>
2022-04-16 21:03:57 +0600

    3 color scheme

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,9 +8,14 @@ $(window).on("load", function() {
 
   var body = $("body");
   var anchors = $("a");
-  var navbar = $("navbar");
+  var navbar = $("nav");
   var navbarImage = $(".navbar-brand img");
+  var navSearch = $(".navbar-nav .nav-item:nth-child(2) .btn");
+  var tabs = $(".nav .nav-link");
+  var tabCards = $(".tab-content > .tab-pane > .card");
+  var tables = $("table.table");
   var footerImage = $("footer img");
+  var formControls = $("input.form-control");
   var bannerImage = $("#banner");
   var getStarted = $("#get-started");
   var isDarkTheme = !body.hasClass("theme-light");
@@ -29,6 +34,15 @@ $(window).on("load", function() {
       footerImage.prop("src", "/assets/img/logo.png");
       getStarted.prop("src", "/assets/img/get-started.svg");
       bannerImage.prop("src", "/assets/img/banner.svg");
+      formControls.removeClass("text-secondary");
+      formControls.addClass("text-light");
+      navSearch.removeClass("text-dark");
+      navSearch.addClass("text-light");
+      tabs.removeClass("bg-light");
+      tabs.addClass("bg-dark");
+      tabCards.removeClass("bg-light");
+      tabCards.addClass("bg-dark");
+      tables.addClass("table-dark");
     } else {
       body.removeClass("text-light");
       body.addClass("theme-light");
@@ -42,6 +56,15 @@ $(window).on("load", function() {
       footerImage.prop("src", "/assets/img/logo-dark.png");
       getStarted.prop("src", "/assets/img/get-started-dark.svg");
       bannerImage.prop("src", "/assets/img/banner-dark.svg");
+      formControls.removeClass("text-light");
+      formControls.addClass("text-secondary");
+      navSearch.addClass("text-dark");
+      navSearch.removeClass("text-light");
+      tabs.removeClass("bg-dark");
+      tabs.addClass("bg-light");
+      tabCards.removeClass("bg-dark");
+      tabCards.addClass("bg-light");
+      tables.removeClass("table-dark");
     }
   }
 
